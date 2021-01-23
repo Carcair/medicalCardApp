02a800f171de7edb85ae5d736f0794eb5257eea8
Carcair <delic.emir90@gmail.com>
2021-01-23 12:06:55 +0100

    First Commit

---
+++ src/test/java/com/emird/medicalCardApp/MedicalCardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emird.medicalCardApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
